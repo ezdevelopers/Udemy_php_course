9e5d93c143d67e03738bdc4112494e6ac17b585d
Emmanuel Eze <emereuwaonueze@gmail.com>
2018-01-27 23:09:20 +0100

    Update files and database

--- includes/db.php
+++ includes/db.php
@@ -2,8 +2,8 @@
 
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
-$db['db_pass'] = "root";
-$db['db_name'] = "cms";
+$db['db_pass'] = "";
+$db['db_name'] = "cms_proj";
 
 foreach($db as $key => $value){
 define(strtoupper($key), $value);
